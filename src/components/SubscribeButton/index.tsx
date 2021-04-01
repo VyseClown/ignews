a030148558fcb6d2f136c95687e4f4e3495bf7dd
Alessandro Marques <vyseclown@gmail.com>
2021-03-31 23:40:03 -0300

    feat: adding the initial subscribe function with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,12 +1,26 @@
-import styles from './styles.module.scss';
+import { useSession, signIn } from "next-auth/client";
+import styles from "./styles.module.scss";
 
-interface SubscribeButtonProps{
-    priceId:string
+interface SubscribeButtonProps {
+  priceId: string;
 }
-export function SubscribeButton({priceId}: SubscribeButtonProps){
-    return (
-        <button type="button" className={styles.subscribeButton}>
-            Subscribe now
-        </button>
-    )
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const [session] = useSession()
+  function handleSubscribe() {
+    if(!session){
+        signIn('github')
+        return;
+    }
+
+    
+  }
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
 }
